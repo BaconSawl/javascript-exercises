91ab35b4c2d4c6531cef185ca547791b3d355e99
BaconSawl <ngolongvu306@gmail.com>
2026-03-06 22:49:35 +0700

    Completed 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
-
+/* The .reverse() method
+const reverseString = function(string) {
+    return string
+            .split("")
+            .reverse()
+            .join("");
 };
+*/
 
+// The loop method
+const reverseString = function(string){
+    let reversedString = "";
+    for (let i = string.length - 1; i >= 0; i--){
+        reversedString += string[i];
+    }
+    return reversedString;
+}
 // Do not edit below this line
 module.exports = reverseString;
