aec9730564d59e7a27dadab0be02e49d8a4bf8e1
BaconSawl <ngolongvu306@gmail.com>
2026-03-24 14:34:34 +0700

    Completed 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, num) => {
+    return sum + num;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((num1, num2) => {
+    return num1 * num2;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let f = 1;
+  for (let i = 0; i < num; i++) {
+    f += f * i;
+  }
+  return f;
 };
 
 // Do not edit below this line
