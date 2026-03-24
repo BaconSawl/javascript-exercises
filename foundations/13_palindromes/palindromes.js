da057cf262e960dde135d82734e6731c2db4d849
BaconSawl <ngolongvu306@gmail.com>
2026-03-24 15:24:36 +0700

    Passed first 3 tests But need to rewrite it, because it works on accident, split(",") will cause a problem later on

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const reversedString = string.split(",").reverse().join("");
+    return (string === reversedString);
 };
 
+/*
+function pld(s) {
+    const reversedString = s.split(",").reverse().join("");
+    return (s == reversedString);
+}
+
+pld("racecar!");
+*/
 // Do not edit below this line
 module.exports = palindromes;
