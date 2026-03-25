3a8cbb753af3698683e02752e27902a4adeb676b
BaconSawl <ngolongvu306@gmail.com>
2026-03-25 15:25:34 +0700

    Completed 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    n = Number(num);
+    if (n < 0) {
+        return ("OOPS");
+    }
+    if (n === 0 || n === 1) {
+        return n;
+    }
+    if (n > 1) {
+        let current = 1;
+        let previous = 0;
+        for (let i = 0; i < n - 1; i++){
+            let next = current + previous;
+            previous = current;
+            current = next;
+            // console.log("Step", i, "previous =", previous, "current =", current, "next =", next);
+        }
+        return current;
+    }
 };
 
+// console.log(fibonacci(6))
 // Do not edit below this line
 module.exports = fibonacci;
