d63b7c720d80c65f59daf093d3d52edc64b47aa6
BaconSawl <ngolongvu306@gmail.com>
2026-03-27 14:29:52 +0700

    Completed 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const currentYear = new Date().getFullYear();
+    const getAge = (person) => {
+        return person.yearOfDeath ?
+        person.yearOfDeath - person.yearOfBirth :
+        currentYear - person.yearOfBirth;
+        ;
+    }
 
+    return arr.reduce((oldest, current) => {
+        if (getAge(oldest) > getAge(current)) {
+            return oldest;
+        } else {
+            return current;
+        }
+    });
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
